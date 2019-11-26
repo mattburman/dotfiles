0cb591da5b32c9ba739c9227cc599f521f88ec97
Matt Burman <matt@mattburman.com>
2019-11-26 14:43:39 +0000

    Configure longer histfile for fzf

---
+++ zsh/lib/fzf.zsh
@@ -0,0 +1,9 @@
+export FZF_DEFAULT_OPTS='--history-size=10000'
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILE=~/.zsh_history
+setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt EXTENDED_HISTORY
+
+bindkey '^[[A' fzf-history-widget
