2e5738142d9af052fbd037f0ababacc8485012ee
Matt Burman <matt@mattburman.com>
2019-12-07 22:53:00 +0000

    Initial hyper-b/e screen switch and tidy use of move

--- phoenix.js
+++ phoenix.js
@@ -1,30 +1,39 @@
 const HYPER = ['ctrl', 'alt', 'cmd', 'shift'];
 const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+const areNumbers = ns => !ns.some(Number.isNaN);
 
 const HIST_LEN = 20;
 const history = new Array(HIST_LEN);
 history.add = v => { history.pop(); history.splice(0, 0, v) };
 
 const move = (getCoords) => {
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
+  const screen = Screen.main();
+  const screenFrame = screen.flippedVisibleFrame();
+  const window = Window.focused();
 
   if (window) {
+    const windowFrame = window.frame();
     const {
-      x = screen.x,
-      y = screen.y,
-      w = window.frame().width,
-      h = window.frame().height,
-    } = getCoords(window, screen);
-    window.setTopLeft({ x, y });
-    window.setSize({ width: w, height: h });
+      x,
+      y,
+      w,
+      h,
+    } = getCoords({ window, screenFrame, screen, windowFrame });
+
+    if (!areNumbers([x, y, w, h])) return console.log('not numbers', {x, y, w, h});
+
+    window.setFrame({
+      x,
+      y,
+      width: w,
+      height: h,
+    });
   }
 };
 
 const hyper = (key, fn) => {
   Key.on(key, HYPER, () => {
     fn();
-    console.log(JSON.stringify(history));
     return history.add(key);
   });
 };
@@ -45,66 +54,103 @@ const thirds = n => {
   }
 };
 const getThirds = pipe(repeats, thirds);
-const getToggled = pipe(repeats, n => !!(n%2));
+const useToggle = pipe(repeats, n => !!(n%2));
 
+const scale = (windowFrame, screenFrame, nextScreenFrame) => {
+  const xProp = windowFrame.width / screenFrame.width;
+  const yProp = windowFrame.height / screenFrame.height;
+
+  // proportions of screen start to window start of the window lengths
+  const xStartProp = (windowFrame.x - screenFrame.x) / screenFrame.width;
+  const yStartProp = (windowFrame.y - screenFrame.y) / screenFrame.height;
+
+  const x = nextScreenFrame.x + nextScreenFrame.width*xStartProp;
+  const y = nextScreenFrame.y + nextScreenFrame.height*yStartProp;
+
+  return {
+    w: xProp * nextScreenFrame.width,
+    h: yProp * nextScreenFrame.height,
+    x,
+    y,
+  };
+};
+
+// v-center to maximise
 hyper('m', () => {
-  const toggled = getToggled('m');
+  const toggled = useToggle('m');
 
-  if (toggled) return move((window, screen) => ({
-    x: screen.x,
-    y: screen.y,
-    w: screen.width,
-    h: screen.height,
+  if (toggled) return move(({ screenFrame }) => ({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    w: screenFrame.width,
+    h: screenFrame.height,
   }));
 
-  return move((window, screen) => ({
-    x: screen.x + (screen.width * 1/3),
-    y: window.frame().y,
-    w: screen.width * (1/3),
-    h: window.height,
+  return move(({ screenFrame, windowFrame }) => ({
+    x: screenFrame.x + (screenFrame.width * 1/3),
+    y: windowFrame.y,
+    w: screenFrame.width * (1/3),
+    h: windowFrame.height,
   }));
 });
+
+hyper('b', () => {
+  move(({ screenFrame, screen, windowFrame }) => scale(
+    windowFrame,
+    screenFrame,
+    screen.previous().flippedVisibleFrame(),
+  ));
+});
+
+hyper('e', () => {
+  move(({ screenFrame, screen, windowFrame }) => scale(
+    windowFrame,
+    screenFrame,
+    screen.next().flippedVisibleFrame(),
+  ));
+});
+
 
 hyper('h', () => {
   const mult = 1 - getThirds('h');
 
-  move((window, screen) => ({
-    x: screen.x,
-    y: screen.y,
-    w: screen.width * mult,
-    h: screen.height,
+  move(({ screenFrame }) => ({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    w: screenFrame.width * mult,
+    h: screenFrame.height,
   }));
 });
 
 hyper('l', () => {
   const mult = getThirds('l');
 
-  move((window, screen) => ({
-    x: screen.x + (mult * screen.width),
-    y: screen.y,
-    w: screen.width * (1-mult),
-    h: screen.height,
+  move(({ screenFrame }) => ({
+    x: screenFrame.x + (mult * screenFrame.width),
+    y: screenFrame.y,
+    w: screenFrame.width * (1-mult),
+    h: screenFrame.height,
   }));
 });
 
 hyper('j', () => {
   const mult = 0.5;
 
-  move((window, screen) => ({
-    x: window.frame().x,
-    y: screen.y + (mult * screen.height),
-    w: window.width,
-    h: screen.height * mult,
+  move(({ screenFrame, windowFrame }) => ({
+    x: windowFrame.x,
+    y: screenFrame.y + (mult * screenFrame.height),
+    w: windowFrame.width,
+    h: screenFrame.height * mult,
   }));
 });
 
 hyper('k', () => {
   const mult = 0.5;
 
-  move((window, screen) => ({
-    x: window.frame().x,
-    y: screen.y,
-    w: window.width,
-    h: screen.height * mult,
+  move(({ screenFrame, windowFrame }) => ({
+    x: windowFrame.x,
+    y: screenFrame.y,
+    w: windowFrame.width,
+    h: screenFrame.height * mult,
   }));
 });
