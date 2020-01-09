4ec1755b6bbcaf94935b04e10634a297e90ad947
Matt Burman <matt@mattburman.com>
2020-01-09 16:40:14 +0000

    Add blog link for reasoning for watch alias

--- zsh/aliases/utils.zsh
+++ zsh/aliases/utils.zsh
@@ -1,6 +1,6 @@
 alias cgrep="grep --color=always"
 alias mk="make"
 
-# does alias expansion
+# does alias expansion https://blog.mact.me/2012/09/06/using-watch-with-a-bash-alias
 alias watch="watch "
 
