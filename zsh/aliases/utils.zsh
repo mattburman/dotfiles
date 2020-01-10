aee9962b8493332905b3132f4ffc45fa0adad0bc
Matt Burman <matt@mattburman.com>
2020-01-10 11:05:54 +0000

    Add -n 1 to watch

--- zsh/aliases/utils.zsh
+++ zsh/aliases/utils.zsh
@@ -2,7 +2,7 @@ alias cgrep="grep --color=always"
 alias mk="make"
 
 # does alias expansion https://blog.mact.me/2012/09/06/using-watch-with-a-bash-alias
-alias watch="watch "
+alias watch="watch -n 1 "
 
 alias ungron="gron -u"
 
