0331194a067b7cbf60364fd926b3231bca5eafaf
Matt Burman <matt@mattburman.com>
2020-01-14 16:05:39 +0000

    LSCOLORS

--- zsh/aliases/utils.zsh
+++ zsh/aliases/utils.zsh
@@ -4,5 +4,8 @@ alias mk="make"
 # does alias expansion https://blog.mact.me/2012/09/06/using-watch-with-a-bash-alias
 alias watch="watch -n 1 "
 
+alias ls="ls --color=auto"
+export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+
 alias ungron="gron -u"
 
