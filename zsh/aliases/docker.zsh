564287d47551c1f1e85580fdcfcd7e3b94a3573e
Matt Burman <matt@mattburman.com>
2020-04-06 14:24:50 +0100

    docker aliases

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -3,3 +3,15 @@
 # remove all containers
 alias dkCnuke="docker rm -v $(docker ps -aq)"
 
+# restart
+alias dkcrs="docker-compose restart"
+
+# up daemonised
+alias dkcud="docker-compose up -d"
+
+# logs
+alias dkclf="docker-compose logs -f"
+alias dkclt="docker-compose logs -t"
+alias dkclft="docker-compose logs -f -t"
+alias dkcltf="docker-compose logs -f -t"
+
