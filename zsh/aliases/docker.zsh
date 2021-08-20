64cc957e0155693ac76d9ed454dfa27c2a8a66db
Matt Burman <matt@mattburman.com>
2021-08-20 16:49:09 +0100

    dkcn alias for generating nfs volume dkc override yamls

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -19,3 +19,23 @@ alias ctop='docker run --rm -ti \
   --name=ctop \
   --volume /var/run/docker.sock:/var/run/docker.sock:ro \
   quay.io/vektorlab/ctop:latest'
+
+dkcn() {
+  local IN=$1
+  local IN=${IN:-docker-compose.yml}
+  local OUT=$2
+  local OUT=${OUT:-nfsvolumes.yml}
+  local VOLBLOCK=$(
+    cat <<-END
+
+    driver: local
+    driver_opts:
+      type: nfs
+      device: ':${PWD}'
+      o: addr=host.docker.internal,rw,nolock,hard,nointr,nfsvers=3
+END
+
+  )
+  echo 'volumes:' >$OUT
+  yq '.volumes | keys[]' $IN | xargs -I'{}' echo "  {}:$VOLBLOCK" >>$OUT
+}
