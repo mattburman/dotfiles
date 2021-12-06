fb5747abdd2a25d4cae9d712ef7ce1e1aea6e99b
Matt Burman <matt@mattburman.com>
2021-12-06 13:30:03 +0000

    some basic macos prefs

--- macos.sh
+++ macos.sh
@@ -1,3 +1,15 @@
+## manual things
+
+# disable cmd-h for required applications in Keyboard > Shortcuts > App Shortcuts
+# e.g. kitty, iterm2
+# disable "Search man page index in Terminal" shift-cmd-A which conflicts with intellij
+# disable spotlight index for things like node_modules in System Preferences > Spotlight > Privacy
+
+## commands
+
+# show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+
 # fast key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 1
@@ -20,5 +32,9 @@ defaults write com.apple.dock static-only -bool true
 
 # cmd-option-d show/hide animation is instant.
 defaults write com.apple.dock autohide-time-modifier -float 0
+
+## quick look
+
+defaults write -g QLPanelAnimationDuration -float 0
 
 killall Dock
